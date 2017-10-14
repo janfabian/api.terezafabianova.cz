bc27497ec149f86a7192a9dbab541f4cadb0e4e3
janfabian <jan.fabian@topmonks.com>
2017-10-14 20:53:55 +0300

    Change email to address

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const utf8 = require("utf8");
 const MIME = (string) => "=?UTF-8?Q?" + qencode.encode(utf8.encode(string)) + "?=";
 
 const config = {
-  targetAddress: "mail@janfabian.cz",
+  targetAddress: "info@terezafabianova.cz",
   fromAddress: MIME("Nová zpráva") + " <mailer@terezafabianova.cz>",
   defaultSubject: "Zpráva od {{name}} na terezafabianova.cz",
   defaultMessage: "Jméno: {{name}}\nEmail: {{email}}\n================\n{{message}}",
